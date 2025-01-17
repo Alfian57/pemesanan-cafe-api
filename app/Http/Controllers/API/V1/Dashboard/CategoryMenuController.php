e75f1d4804b61874412412ba30320f70a0befc17
Alfian57 <alfiangadingsaputra@gmail.com>
2025-01-17 21:26:26 +0700

    feat: attach and detach category from menu api feature

--- app/Http/Controllers/API/V1/Dashboard/CategoryMenuController.php
+++ app/Http/Controllers/API/V1/Dashboard/CategoryMenuController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers\API\V1\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Menu;
+use Illuminate\Http\Request;
 
 class CategoryMenuController extends Controller
 {
-    //
+    /**
+     * Summary of addCategoryToMenu
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function addCategoryToMenu(Request $request, Menu $menu, Category $category)
+    {
+        $menu->categories()->attach($category->id);
+
+        return response()->json(['message' => 'Category added to menu']);
+    }
+
+    /**
+     * Summary of removeCategoryFromMenu
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function removeCategoryFromMenu(Menu $menu, Category $category)
+    {
+        $menu->categories()->detach($category->id);
+
+        return response()->json(['message' => 'Category removed from menu']);
+    }
 }
